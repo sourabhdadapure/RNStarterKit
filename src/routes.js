145a8e5cb9e6503ab917f52b9e0f2a9beeeebf3b
sourabhdadapure <sourabhdadapure@gmail.com>
2019-03-01 00:13:39 -0500

    Adds redux boilerplate

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { createAppContainer, createStackNavigator } from 'react-navigation'
 import { Splash, LogIn } from './screens'
+
+import configureStore from './config/configureStore'
+
+const store = configureStore()
 
 const AppStack = createStackNavigator({
   Splash: { screen: Splash },
@@ -11,6 +16,10 @@ const App = createAppContainer(AppStack)
 
 export default class Routes extends React.Component {
   render() {
-    return <App />
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
   }
 }
