531d60417403b91b5c5ec632db41e826b58fb842
sourabhdadapure <sourabhdadapure@gmail.com>
2019-03-01 19:42:53 -0500

    insertions

--- src/screens/Splash/component.js
+++ src/screens/Splash/component.js
@@ -2,19 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
 `
 
 const SplashText = styled.Text`
-  font-size: 15;
+	font-size: 15;
 `
 
 const SplashComponent = () => (
-  <Wrapper>
-    <SplashText>Splash Screen</SplashText>
-  </Wrapper>
+	<Wrapper>
+		<SplashText>Splash Screen</SplashText>
+	</Wrapper>
 )
 
 export default SplashComponent
