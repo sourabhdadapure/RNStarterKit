612a752f444a5f58a0041155f813512d7f5c0b60
sourabhdadapure <sourabhdadapure@gmail.com>
2019-03-01 19:38:50 -0500

    Adds linter config

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import styled from 'styled-components'
+import PropTypes from 'prop-types'
 import { Button } from 'react-native-elements'
 
 const NavButton = ({ loading, onPress, title }) => (
-  <Button onPress={onPress} loading={loading} title={title} />
+	<Button onPress={onPress} loading={loading} title={title} />
 )
+
+NavButton.defaultProps = {
+	loading: false,
+}
+
+NavButton.propTypes = {
+	loading: PropTypes.bool,
+	onPress: PropTypes.func.isRequired,
+	title: PropTypes.string.isRequired,
+}
 
 export default NavButton
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import { createAppContainer, createStackNavigator } from 'react-navigation'
 import { Splash, LogIn } from './screens'
 
 const AppStack = createStackNavigator({
-  Splash: { screen: Splash },
-  LogIn: { screen: LogIn }
+	Splash: { screen: Splash },
+	LogIn: { screen: LogIn },
 })
 
 const App = createAppContainer(AppStack)
 
 export default class Routes extends React.Component {
-  render() {
-    return <App />
-  }
+	render() {
+		return <App />
+	}
 }
